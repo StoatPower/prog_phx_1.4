85322f9902d0ec2c58d8e96fce10f640beb0d414
Matt Caldwell <mattcaldwell84@gmail.com>
2021-02-21 19:35:39 -0500

    much work on channels and presence

--- rumbl/lib/rumbl_web/controllers/auth.ex
+++ rumbl/lib/rumbl_web/controllers/auth.ex
@@ -18,11 +18,11 @@ defmodule RumblWeb.Auth do
     # matter how it got there. We have an improved testing story that doesn’t require
     # us to write mocks or any other elaborate scaffolding.
     cond do
-      conn.assigns[:current_user] ->
-        conn
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
 
       user = user_id && Rumbl.Accounts.get_user(user_id) ->
-        assign(conn, :current_user, user)
+        put_current_user(conn, user)
 
       true ->
         assign(conn, :current_user, nil)
@@ -31,9 +31,17 @@ defmodule RumblWeb.Auth do
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true) #protects from session fixation attacks
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
   def logout(conn) do
